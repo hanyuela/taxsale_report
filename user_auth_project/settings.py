7df6317b10f2fe9596eb3ef44efa6901cc275bd7
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-11-06 17:51:40 +0800

    Add files via upload

--- user_auth_project/settings.py
+++ user_auth_project/settings.py
@@ -91,8 +91,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/assets/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),  # 拼接 BASE_DIR 和 assets 文件夹路径
+]
 LOGIN_URL = '/login/'
 
