93075ed55d8817c890f7ac88dc241b13ea321671
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-11-06 17:47:44 +0800

    Add files via upload

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('template/index.html', views.template_index, name='template_index'),  # 新增路径
     path('edit-profile/', views.edit_profile, name='edit-profile'),
     path('datatable/', views.datatable, name='datatable'),
-    path('forget-password/', views.forget_password, name='forget_password'),
     path('error/', views.error_503, name='error_503'),
     path('sign-up-wizard/', views.signup_wizard, name='sign-up-wizard'),
-    path('confirm-password-reset/<int:user_id>/<uuid:token>/', views.confirm_password_reset, name='confirm_password_reset'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('request_password_reset/', views.request_password_reset, name='request_password_reset'),
+    path('reset/<uidb64>/<token>/', views.reset_password, name='reset_password'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -14,6 +14,9 @@ from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formataddr
 import smtplib
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
 # 注册页面
 def register(request):
     if request.method == 'POST':
@@ -57,7 +60,7 @@ def index(request):
     if request.user.is_authenticated:
         return render(request, 'index.html')  # 用户已登录，渲染 index.html
     else:
-        return render(request, 'base.html')  # 用户未登录，渲染 base.html
+        return render(request, 'login.html')  # 用户未登录，渲染 base.html
 
 
 # 编辑个人资料页面
@@ -106,105 +109,79 @@ def signup_wizard(request):
 def template_index(request):
     return render(request, 'template/index.html')  # 渲染 template/index.html
 
-def forget_password(request):
-    if request.method == "POST":
-        email = request.POST.get('email')  # 获取用户输入的电子邮件
-        new_password = request.POST.get('new_password')  # 获取用户输入的新密码
 
-        # 检查电子邮件和新密码的有效性
-        if not email or not new_password:
-            messages.error(request, 'Please enter a valid email and password.')
-            return redirect('forget-password')
+
+# 处理第一步：请求重置密码链接
+def request_password_reset(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        
+        # 检查邮箱是否在数据库中
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            # 如果没有找到该邮箱，返回错误消息
+            messages.error(request, 'Email address does not exist in our system.')
+            return redirect('request_password_reset')
+        
+       # 生成重置链接
+        token = default_token_generator.make_token(user)
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        reset_link = request.build_absolute_uri(f"/reset/{uid}/{token}/")
+        
+        # 使用SMTP发送邮件
+        subject = 'Password Reset Request'
+        body = f'Click the link to reset your password: {reset_link}'
+
+        # 构建 MIME 邮件
+        sender_email = settings.EMAIL_HOST_USER
+        receiver_email = email
+        msg = MIMEText(body, 'plain', 'utf-8')
+        msg['From'] = formataddr((str(Header("Soyhome.app", 'utf-8')), sender_email))
+        msg['To'] = receiver_email
+        msg['Subject'] = Header(subject, 'utf-8')
 
         try:
-            user = User.objects.get(email=email)  # 获取用户
-            user.set_password(new_password)  # 更新用户密码
-            user.save()  # 保存更新
+            smtp_server = settings.EMAIL_HOST
+            smtp_port = settings.EMAIL_PORT
+            smtp_user = settings.EMAIL_HOST_USER
+            smtp_password = settings.EMAIL_HOST_PASSWORD
+
+            with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+                server.login(smtp_user, smtp_password)
+                server.sendmail(sender_email, [receiver_email], msg.as_string())
             
-            # 生成一个唯一的令牌并保存到用户的 UserProfile 中
-            token = uuid.uuid4()
-            user_profile, created = UserProfile.objects.get_or_create(user=user)
-            user_profile.password_reset_token = token
-            user_profile.is_password_reset_confirmed = False  # 设置为未确认
-            user_profile.save()
-
-            # 构建确认链接
-            subject = "Soyhome.app - Password Reset Confirmation"
-            body = f"Hello, please click on the following link to confirm your password reset: {request.scheme}://{request.get_host()}/confirm-password-reset/{user.id}/{token}/"
-
-            # 构建 MIME 邮件
-            sender_email = settings.EMAIL_HOST_USER
-            receiver_email = user.email
-            msg = MIMEText(body, 'plain', 'utf-8')
-            msg['From'] = formataddr((str(Header("Soyhome.app", 'utf-8')), sender_email))
-            msg['To'] = receiver_email
-            msg['Subject'] = Header(subject, 'utf-8')
-
-            # 使用 SMTP 发送邮件
-            try:
-                smtp_server = settings.EMAIL_HOST
-                smtp_port = settings.EMAIL_PORT
-                smtp_user = settings.EMAIL_HOST_USER
-                smtp_password = settings.EMAIL_HOST_PASSWORD
-
-                with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
-                    server.login(smtp_user, smtp_password)
-                    server.sendmail(sender_email, [receiver_email], msg.as_string())
-                
-                messages.success(request, 'A confirmation email has been sent to your email address.')
-                return redirect('/login/')  # 重定向到登录页面
-            except Exception as e:
-                messages.error(request, f'An error occurred while sending the message: {e}')
-                return redirect('forget-password')
-
-        except User.DoesNotExist:
-            messages.error(request, 'No account found with this email address.')
-            return redirect('forget-password')
-
-    # GET 请求，渲染忘记密码页面
-    return render(request, 'forget-password.html')  # 渲染忘记密码模板
+            messages.success(request, 'Password reset link has been sent to your email.')
+            return redirect('request_password_reset')
+        except Exception as e:
+            messages.error(request, f'An error occurred while sending the message: {e}')
+            return redirect('request_password_reset')
+    
+    return render(request, 'request_password_reset.html')
 
 
-
-def confirm_password_reset(request, user_id, token):
-    print("Confirm password reset view called")  # 确认视图是否被调用
+def reset_password(request, uidb64, token):
     try:
-        # 获取用户对象
-        user = User.objects.get(id=user_id)
-        user_profile = UserProfile.objects.get(user=user)
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
 
-        # 打印调试信息，查看用户和令牌
-        print(f"User ID: {user_id}, Token in URL: {token}")
-        print(f"Token in Database: {user_profile.password_reset_token}")
-
-        # 将 token 转换为字符串并进行比较
-        if str(user_profile.password_reset_token) == str(token):
-            print("Token matches, proceeding to update...")  # 验证令牌匹配
-
-            # 更新字段
-            user_profile.is_password_reset_confirmed = True
-            user_profile.password_reset_token = None
-
-            # 尝试保存更改，并捕获保存成功或失败的信息
-            try:
-                user_profile.save()  # 尝试保存更改
-                print("Password reset confirmed:", user_profile.is_password_reset_confirmed)  # 保存成功
-                messages.success(request, 'Your password has been successfully updated.')
-            except Exception as e:
-                # 捕获保存错误并输出
-                print("Error saving user profile:", e)
-                messages.error(request, 'Failed to update profile. Please try again later.')
-        else:
-            print("Token does not match.")  # 如果令牌不匹配
-            messages.error(request, 'Password reset link is invalid or expired.')
-    except User.DoesNotExist:
-        print("User does not exist.")  # 用户不存在
-        messages.error(request, 'User does not exist.')
-    except UserProfile.DoesNotExist:
-        print("User profile does not exist.")  # 用户配置文件不存在
-        messages.error(request, 'User profile does not exist.')
-
-    return redirect('/login/')
+    if user is not None and default_token_generator.check_token(user, token):
+        if request.method == 'POST':
+            new_password = request.POST['new_password']
+            confirm_password = request.POST['confirm_password']
+            if new_password == confirm_password:
+                user.set_password(new_password)
+                user.save()
+                messages.success(request, 'Your password has been reset successfully.')
+                return redirect('login')
+            else:
+                messages.error(request, 'Passwords do not match.')
+        return render(request, 'forget-password.html', {'uidb64': uidb64, 'token': token})
+    else:
+        messages.error(request, 'Invalid or expired reset link.')
+        return redirect('request_password_reset')
 
 
 def dashboard(request):
