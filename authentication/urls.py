526f350df1b20ba56d133a62385ba3841f95e5ea
hanyuela <65677970+hanyuela@users.noreply.github.com>
2024-11-13 22:14:46 -0800

    Update urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('index/', views.index, name='index'),  # 访问主页后跳转到 index 页面
-    path('template/index.html', views.template_index, name='template_index'),
     path('edit-profile/', views.edit_profile, name='edit-profile'),
     path('datatable/', views.datatable, name='datatable'),
     path('error/', views.error_503, name='error_503'),
