eaa4e60b0b459d329ef478ee7874419210c1bdab
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-11-22 17:49:04 +0800

    Add files via upload

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    GOAL_CHOICES = [
+        ('interest', 'Interest'),
+        ('foreclosure', 'Foreclosure'),
+    ]
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")  # 关联 User 表
     first_name = models.CharField(max_length=50, blank=True, null=True)  # 名（可为空）
     last_name = models.CharField(max_length=50, blank=True, null=True)  # 姓（可为空）
@@ -9,6 +13,8 @@ class UserProfile(models.Model):
     investment_amount = models.DecimalField(
         max_digits=12, decimal_places=2, blank=True, null=True
     )  # 投资金额（可为空）
-
+    goal = models.CharField(
+        max_length=15, choices=GOAL_CHOICES, null=True, blank=True, default=""
+    )
     def __str__(self):
         return f"{self.user.username}'s Profile"
