2cf306f3daf2def10166031b9bb416debbe73736
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-11-27 16:05:57 +0800

    Add files via upload

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator
 
 class UserProfile(models.Model):
     GOAL_CHOICES = [
@@ -11,10 +12,15 @@ class UserProfile(models.Model):
     last_name = models.CharField(max_length=50, blank=True, null=True)  # 姓（可为空）
     phone_number = models.CharField(max_length=15, blank=True, null=True)  # 手机号码（可为空）
     investment_amount = models.DecimalField(
-        max_digits=12, decimal_places=2, blank=True, null=True
-    )  # 投资金额（可为空）
+        max_digits=12,
+        decimal_places=2,
+        blank=True,
+        null=True,
+        validators=[MaxValueValidator(1000000000)]
+    ) # 投资金额（可为空）
     goal = models.CharField(
         max_length=15, choices=GOAL_CHOICES, null=True, blank=True, default=""
     )
+    
     def __str__(self):
         return f"{self.user.username}'s Profile"
