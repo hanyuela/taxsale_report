f450ed403d05301225ceaf4d06d630f9dbf54bb2
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-12-18 21:20:16 +0800

    Add files via upload

--- user_auth_project/settings.py
+++ user_auth_project/settings.py
@@ -99,3 +99,9 @@ STATICFILES_DIRS = [
 ]
 LOGIN_URL = '/login/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tech@soyhome.app'
+EMAIL_HOST_PASSWORD = 'Soyhome@us2021!'
