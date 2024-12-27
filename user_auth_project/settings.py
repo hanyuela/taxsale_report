ddda66ffac85719735d2dd9d3eebf9e1debb0283
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2024-12-27 18:46:19 +0800

    Add files via upload

--- user_auth_project/settings.py
+++ user_auth_project/settings.py
@@ -68,6 +68,8 @@ DATABASES = {
     }
 }
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
