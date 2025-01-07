58f3f3d6565e41e8062c2ba7fcd84f156b74ad9a
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2025-01-07 17:07:25 +0800

    Add files via upload

--- authentication/models.py
+++ authentication/models.py
@@ -3,26 +3,31 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator
 
 class UserProfile(models.Model):
-    GOAL_CHOICES = [
-        ('interest', 'Interest'),
-        ('foreclosure', 'Foreclosure'),
+    # 定义会员类型
+    MEMBERSHIP_CHOICES = [
+        (0, 'Non-Paid User'),
+        (1, 'Monthly Paid User'),
+        (2, 'Annually Paid User'),
     ]
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")  # 关联 User 表
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     avatar_path = models.CharField(max_length=255, blank=True, null=True)
-    first_name = models.CharField(max_length=50, blank=True, null=True)  # 名（可为空）
-    last_name = models.CharField(max_length=50, blank=True, null=True)  # 姓（可为空）
-    phone_number = models.CharField(max_length=15, blank=True, null=True)  # 手机号码（可为空）
-    default = models.BooleanField(default=False)  # 是否默认同意付费，无需再次提示s
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    default = models.BooleanField(default=False)  # 是否默认同意付费，无需再次提示
     investment_amount = models.DecimalField(
         max_digits=12,
         decimal_places=2,
         blank=True,
         null=True,
         validators=[MaxValueValidator(1000000000)]
-    ) # 投资金额（可为空）
-    goal = models.CharField(
-        max_length=15, choices=GOAL_CHOICES, null=True, blank=True, default=""
-    )
+    )  # 投资金额（可为空）
+    goal = models.CharField(max_length=15, null=True, blank=True, default="")
+    
+    # 新增字段
+    member = models.IntegerField(choices=MEMBERSHIP_CHOICES, default=1)  # 会员类型，默认为按月付费用户
+    member_start = models.DateTimeField(null=True, blank=True)  # 会员开始时间
     
     def __str__(self):
         return f"{self.user.username}'s Profile"
