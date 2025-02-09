237bf6b7fb8114ed51dff305ed8769ef2ba64298
GaLimonkey <68153022+GaLimonkey@users.noreply.github.com>
2025-02-09 21:47:17 +0800

    Add files via upload

--- authentication/urls.py
+++ authentication/urls.py
@@ -55,6 +55,7 @@ urlpatterns = [
     path('invoice/',payments_views.invoice,name='invoice'),
     path('subscription-trial/',views.subscription_trial,name='subscription_trial'),
     path('cancel-subscription/', payments_views.cancel_subscription, name='cancel_subscription'),
+    path('apply-coupon/', payments_views.apply_coupon, name='apply_coupon'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
